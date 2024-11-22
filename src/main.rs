d20ca9e03f0317659d94b9ca722b00767676fde6
pelmeshke <75571195+pelmesh619@users.noreply.github.com>
2024-11-23 02:14:18 +0300

    feat some main.rs draft

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,31 @@
 use std::env;
 
-struct ProgramInput {
-    input_filename: String
-}
-
-impl ProgramInput {
-    pub fn parse(arguments: &Vec<String>) {
-
-    }
-}
+mod parser;
+use parser::{ParseResult, ProgramInput};
+mod splimer;
+use splimer::Splimer;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     dbg!(&args);
 
     let program_input = ProgramInput::parse(&args);
+
+    match program_input {
+        ParseResult::ThereIsNoInputFilename => {
+            eprintln!("There is no input filename in arguments!");
+            return;
+        },
+        ParseResult::MemoryValueCannotBeParsed(string) => {
+            eprintln!("This string \"{}\" cannot be parsed as memory value", string);
+            return;
+        },
+        _ => { }
+    }
+
+    let ParseResult::Success(program_input) = program_input else { panic!(); };
+
+    let mut splimer = Splimer{program_input};
+
+    splimer.work();
 }
